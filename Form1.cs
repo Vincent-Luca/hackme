dc95276bdcde3c0821076d7f149c43396a5f4f4d
AnimeTheNeko <56701097+AnimeTheNeko@users.noreply.github.com>
2021-10-24 23:55:53 +0200

    very much still a work in progress

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,74 @@ namespace hackme
 {
     public partial class Form1 : Form
     {
+        string password;
+        string guesspass;
+        string alph = "abcdefghijklmnopqrstuvwxyz0123456789";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "")
+            {
+                //int count = 0;
+                /*
+                foreach (char item in textBox2.Text)
+                {
+                    count++;
+                }
+                if ()
+                {
+
+                }
+                */
+                password = textBox2.Text;
+            }
+            else
+            {
+                MessageBox.Show("You have to put something in");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (password == guesspass)
+            {
+                MessageBox.Show("You did it!");
+            }
+            else
+            {
+                MessageBox.Show("Somthing went wrong! password incorrect");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            guesspass = "";
+            int i = 0;
+            int count = 0;
+            foreach (char letter in alph)
+            {
+                if (i != alph.Length)
+                {
+                    guesspass = letter.ToString();
+                    //int index = guesspass.LastIndexOf(letter);
+                    textBox1.Text = guesspass;
+                    guesspass.Substring(0, count);
+                    if (guesspass == password)
+                    {
+                        break;
+                    }
+                    i++;
+                }
+                else
+                {
+                    i = 0;
+                    count++;
+                }
+            }
         }
     }
 }
